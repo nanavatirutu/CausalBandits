f741249098ff4434108ceac3f878f12000852912
Akshay Suresh <suresh.aks@husky.neu.edu>
2019-07-16 10:34:27 -0400

    new folder structure

--- CausalBandits/python_boilerplate/python_boilerplate/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for Python Boilerplate."""
-
-__author__ = """Rutu"""
-__email__ = 'rutunanavati95@gmail.com'
-__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A causal approach to multi-armed bandit problems with observed and unobserved confounders',
+    author='Akshay Suresh; Ankitha Moodukudru; Rutu Nanavati',
+    license='',
+)
